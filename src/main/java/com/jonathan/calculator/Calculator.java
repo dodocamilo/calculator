ee90e14d9fba877b00a36c37afdc6dcae2ea9621
Jonathan Camilo <dodocamilo@gmail.com>
2019-02-12 14:45:48 -0200

    Adding checkstyle

--- src/main/java/com/jonathan/calculator/Calculator.java
+++ src/main/java/com/jonathan/calculator/Calculator.java
@@ -2,6 +2,9 @@ package com.jonathan.calculator;
 
 import org.springframework.stereotype.Service;
 
+/**
+* Calculator service.
+*/
 @Service
 public class Calculator {
 	public int sum(int a, int b) {
